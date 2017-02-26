f263f77d198757d5707a836799c6a232c486565b
Gerrald Mitchell <bluelionjr@gmail.com>
2017-02-26 16:21:42 -0500

    Assignment 7 Complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,13 +1,21 @@
 (function() {
     function SongPlayer() {
         var SongPlayer = {};
-
+        
+        // Private attributes
+        /**
+        * @desc Current song variable
+        * @type {Object}
+        */
         var currentSong = null;
+        
         /**
         * @desc Buzz object audio file
         * @type {Object}
         */
         var currentBuzzObject = null;
+        
+        // Private functions
         /**
         * @function setSong
         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
@@ -27,13 +35,28 @@
             currentSong = song;
         };
         
+        /**
+        * @function playSong
+        * @desc Plays the loaded currentBuzzObject, private function
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        
+        
+        // Public methods
+        /**
+        * @function SongPlayer.play - public method of the SongPlayer service
+        * @desc Uses the private setSong and playSong methods to play music one at a time
+        * @param {Object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 
                 setSong(song);
-            
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } 
             
             else if (currentSong === song) {
@@ -43,6 +66,11 @@
             }
         };
         
+        /**
+        * @function SongPlayer.pause - public method of the SongPlayer service
+        * @desc Pauses the currently playing song, stets song.playing to false
+        * @param {Object} song
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
