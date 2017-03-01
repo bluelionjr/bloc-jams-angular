86a1ec1c1d0a1b5d4cdf75ed6f3685471b680b20
Gerrald Mitchell <bluelionjr@gmail.com>
2017-03-01 07:47:21 -0500

    Checkpoint 9 Complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -7,7 +7,7 @@
         * @desc the list of songs on the album
         * @type {Object}
         */
-        var currentAlbum = Fixtures.getAlbum();
+        var currentAlbum = Fixtures.getAlbum(); 
         
 
         /**
@@ -142,5 +142,99 @@
     
     angular
         .module('blocJams')
-        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
+        .factory('SongPlayer', ['Fixtures', SongPlayer]);
 })();
+
+// Start Mark
+
+
+//(function(){
+//  function SongPlayer(Fixtures) {
+//    var SongPlayer = {};
+//    var currentAlbum = Fixtures.getAlbum();
+//
+//    /**
+//    * @desc Buzz object audio file
+//    * @type {Object}
+//    */
+//    var currentBuzzObject = null; //sets up currentBuzzObject var
+//
+//    /**
+//    * @function setSong
+//    * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+//    * @param {object} song
+//    */
+//    var setSong = function(song) { //private method
+//      if (currentBuzzObject) {
+//        currentBuzzObject.stop();
+//        SongPlayer.currentSong.playing = null;
+//      }
+//
+//      currentBuzzObject = new buzz.sound(song.audioUrl, {
+//        formats: ['mp3'],
+//        preload: true
+//      });
+//      SongPlayer.currentSong = song;
+//
+//    };
+//
+//    /**
+//    * @desc playSong plays current Buzz object
+//    @function playSong
+//    * @param {object} song
+//    */
+//    var playSong = function(song){
+//      currentBuzzObject.play();
+//      song.playing = true;
+//    };
+//
+//    var getSongIndex = function(song) {
+//      return currentAlbum.songs.indexOf(song);
+//    };
+//    //public method
+//
+//    /**
+//    * @desc initialize currentSong var to be null
+//    */
+//    SongPlayer.currentSong = null; //sets up currentSong public attribute
+//
+//    SongPlayer.play = function(song) { //object. Question: Can I do this.play = function(song) instead of SongPlayer.play?
+//        song = song || SongPlayer.currentSong;
+//        if (SongPlayer.currentSong !== song) { //if currently playing song is not the song that is chosen
+//            setSong(song);
+//            playSong(song);
+//        } else if (SongPlayer.currentSong === song) { //else if we are hovering on song #2 and clicks song#2 (2 possibility: is song#2 playing? is song#2 paused?)
+//          if (currentBuzzObject.isPaused()) { //if it is paused, then play it
+//            playSong(song);
+//          }
+//        }
+//    };
+//
+//    //public method
+//    SongPlayer.pause = function(song) {
+//      song = song || SongPlayer.currentSong;
+//      currentBuzzObject.pause();
+//      song.playing = false;
+//    }
+//
+//    SongPlayer.previous = function(){
+//      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+//      currentSongIndex--;
+//
+//      if (currentSongIndex < 0) {
+//        currentBuzzObject.stop();
+//        SongPlayer.currentSong.playing = null;
+//      } else {
+//        var song = currentAlbum.songs[currentSongIndex];
+//        setSong(song);
+//        playSong(song);
+//      }
+//    };
+//
+//    return SongPlayer;
+//  }
+//
+//  angular
+//    .module('blocJams')
+//    .factory('SongPlayer', SongPlayer);
+//})();
